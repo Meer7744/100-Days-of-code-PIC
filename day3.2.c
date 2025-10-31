6c6552173d24cea6e6fc5a846b579fdc73e49af8
Meer7744 <meersingh471@gmail.com>
2025-10-31 18:32:36 +0530

    day3.2.c

---
+++ day3.2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    // Input two numbers
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    // Display before swapping
