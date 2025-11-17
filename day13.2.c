e89bc173c162a621d860733f8d6b01abba561128
Meer7744 <meersingh471@gmail.com>
2025-11-17 19:57:34 +0530

    day13.2.c

---
+++ day13.2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    // Input the value of n
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    // Print numbers from 1 to n
+    printf("Numbers from 1 to %d are:\n", n);
+    for (i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    return 0;
+}
