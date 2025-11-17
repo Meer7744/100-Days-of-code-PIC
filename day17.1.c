51f7615d182c12afb1ad21b4213bd50b7e47fb62
Meer7744 <meersingh471@gmail.com>
2025-11-17 20:02:30 +0530

    Create day17.1.c

---
+++ day17.1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, original, remainder, n = 0;
+    double sum = 0;
+
+    // Input number
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    // Count number of digits
+    int temp = num;
+    while (temp != 0) {
+        temp /= 10;
+        n++;
+    }
+
+    // Calculate sum of each digit raised to power n
+    temp = num;
+    while (temp != 0) {
+        remainder = temp % 10;
+        sum += pow(remainder, n);
+        temp /= 10;
+    }
+
+    // Check if sum equals original number
+    if ((int)sum == original) {
+        printf("%d is an Armstrong number.\n", original);
+    } else {
+        printf("%d is not an Armstrong number.\n", original);
+    }
+
+    return 0;
+}
