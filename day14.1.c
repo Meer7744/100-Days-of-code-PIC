57899e510cd7965310b2ad0539c1ec11abd527fa
Meer7744 <meersingh471@gmail.com>
2025-11-17 19:58:21 +0530

    day14.1.c

---
+++ day14.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    // Input the number of odd numbers
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    // Calculate sum of first n odd numbers
+    for (int i = 1; i <= 2*n; i += 2) {
+        sum += i;
+    }
+
+    printf("Sum of the first %d odd numbers = %d\n", n, sum);
+
+    return 0;
+}
