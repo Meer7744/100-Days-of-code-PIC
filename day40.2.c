c8c8e673a35abcb2f1766bb3d0483e3bcc5bbd2f
Meer7744 <meersingh471@gmail.com>
2025-11-19 19:12:29 +0530

    day40.2.c

---
+++ day40.2.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main() {
+    int r1, c1, r2, c2;
+
+    // Read dimensions
+    printf("Enter rows and columns of Matrix A: ");
+    scanf("%d %d", &r1, &c1);
+
+    printf("Enter rows and columns of Matrix B: ");
+    scanf("%d %d", &r2, &c2);
+
+    // Check multiplication condition
+    if (c1 != r2) {
+        printf("Matrix multiplication not possible! (c1 != r2)\n");
+        return 0;
+    }
+
+    int A[r1][c1], B[r2][c2], C[r1][c2];
+
+    // Read Matrix A
+    printf("Enter elements of Matrix A:\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Read Matrix B
+    printf("Enter elements of Matrix B:\n");
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Initialize result matrix C with zeros
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            C[i][j] = 0;
+        }
+    }
+
+    // Matrix multiplication
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            for (int k = 0; k < c1; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    // Print Result Matrix
+    printf("\nResultant Matrix (A × B):\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
