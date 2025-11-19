8fe3f885a51408e54ebf59cb6624d30746c2ef4d
Meer7744 <meersingh471@gmail.com>
2025-11-19 19:09:10 +0530

    day38.1.c

---
+++ day38.1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int A[rows][cols], B[rows][cols], Sum[rows][cols];
+
+    // Read first matrix
+    printf("Enter elements of Matrix A:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Read second matrix
+    printf("Enter elements of Matrix B:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Matrix addition
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            Sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    // Print result
+    printf("\nSum of the two matrices:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", Sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
