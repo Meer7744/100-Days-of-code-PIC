7f619d4eed2217917a0eea3685eae7328c73cf04
Meer7744 <meersingh471@gmail.com>
2025-11-19 18:50:03 +0530

    day24.1.c

---
+++ day24.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 5; i++) {        // rows
+        for (int j = 1; j <= i; j++) {    // print stars equal to row number
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
