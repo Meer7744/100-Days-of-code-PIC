57679a532b49544f445e23c3a6a14f836da4c71d
Meer7744 <meersingh471@gmail.com>
2025-11-19 19:11:50 +0530

    day40.1.c

---
+++ day40.1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+
+    // Read matrix elements
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("\nDiagonal Traversal:\n");
+
+    // Step 1: Start from the first row
+    for (int startCol = 0; startCol < cols; startCol++) {
+        int i = 0, j = startCol;
+        while (i < rows && j >= 0) {
+            printf("%d ", matrix[i][j]);
+            i++;
+            j--;
+        }
+        printf("\n");
+    }
+
+    // Step 2: Start from the last column downward
+    for (int startRow = 1; startRow < rows; startRow++) {
+        int i = startRow, j = cols - 1;
+        while (i < rows && j >= 0) {
+            printf("%d ", matrix[i][j]);
+            i++;
+            j--;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
