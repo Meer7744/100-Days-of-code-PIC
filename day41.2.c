0ca32f54a3748fecf953609983a8bceaf5ffb34e
Meer7744 <meersingh471@gmail.com>
2025-11-20 22:04:14 +0530

    day41.2.c

---
+++ day41.2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Print each character on a new line
+    while (str[i] != '\0') {
+        if (str[i] != '\n') {  // avoid printing newline from fgets
+            printf("%c\n", str[i]);
+        }
+        i++;
+    }
+
+    return 0;
+}
